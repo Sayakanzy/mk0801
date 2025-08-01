4e58b7d3599b0ffef197ce9c5ef0ec22f0e1e2d2
Sayaka ANZAI <sayaka-imac@SayakaiMacPurple.local>
2025-08-01 10:24:11 +0900

    fix: add firebase dependency

--- firebase.ts
+++ firebase.ts
@@ -3,12 +3,12 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDl5MWuK9fKC22YXI8woC6LavoRB_zZQyI",
-  authDomain: "kenshin0731-66cd9.firebaseapp.com",
-  projectId: "kenshin0731-66cd9",
-  storageBucket: "kenshin0731-66cd9.firebasestorage.app",
-  messagingSenderId: "529311224966",
-  appId: "1:529311224966:web:b6d0786694aee6f5efad07"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
